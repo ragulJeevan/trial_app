ee92d72ec2544e802c840ccc1bde991db8cc8995
Ragul <ragul@cloudvalleytech.com>
2023-07-02 12:48:07 +0530

    minor commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,10 @@ export const environment = {
   userName : ['8248399209','9159876526'],
   passCode : ['ragul','nawin'],
 };
+
+
+// ************************* TO PUBLISH IN GIT HUB PAGES  ***************//
+// 1)npm intsall -g angular-cli-ghpages
+// 2)mg build --prod base-href"https://username.gituhub.io/project-name/ ".
+// 3)ngh --dir dist/project-name.
+// ******************************************************************* //
